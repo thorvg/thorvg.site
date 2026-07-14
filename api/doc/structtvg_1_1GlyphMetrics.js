a8fa14c85376defa8ba546a21d8d09671aa133df
Hermet Park <hermetpark@gmail.com>
2026-07-14 13:23:08 +0900

    api: added missing files

---
+++ api/doc/structtvg_1_1GlyphMetrics.js
@@ -0,0 +1,7 @@
+var structtvg_1_1GlyphMetrics =
+[
+    [ "advance", "structtvg_1_1GlyphMetrics.html#a19bfad41ad2f8797160dcd720cadc2a5", null ],
+    [ "bearing", "structtvg_1_1GlyphMetrics.html#af6eb7f864211ea48ab64c74d4f69fa41", null ],
+    [ "max", "structtvg_1_1GlyphMetrics.html#a22b300c2a2926fa66c22fdcfb04c3dd7", null ],
+    [ "min", "structtvg_1_1GlyphMetrics.html#acb9e23847ba1a103e513e12be3316dc3", null ]
+];
---
+++ api/doc/structtvg_1_1TextMetrics.js
@@ -0,0 +1,7 @@
+var structtvg_1_1TextMetrics =
+[
+    [ "advance", "structtvg_1_1TextMetrics.html#a19bfad41ad2f8797160dcd720cadc2a5", null ],
+    [ "ascent", "structtvg_1_1TextMetrics.html#a31ebc425eeae55d11acd591bb46dd468", null ],
+    [ "descent", "structtvg_1_1TextMetrics.html#a5984964357202018e67f64fd879af95b", null ],
+    [ "linegap", "structtvg_1_1TextMetrics.html#af5dce933ea3f84ac856b48d0f05f3fc6", null ]
+];
